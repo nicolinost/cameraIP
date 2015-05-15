5bc5cb0915aa326bd444cd69cbd3563a750ed302
nicolinost <nicolassterchele@itsyours.local>
2015-05-15 21:26:59 +0200

    For test

--- cameraIP/ViewController.swift
+++ cameraIP/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //It will works one day !!!
     }
 
     override func didReceiveMemoryWarning() {
